33f6ecf09070816e20fdc5361c0d532563fb94f6
Juan Manuel Giraldo <juanmanuelgiraldoc@gmail.com>
2025-09-26 22:35:05 -0500

    Progress on binarization

--- binaryzation.py
+++ binaryzation.py
@@ -1,23 +1,26 @@
 import cv2
-from cv2.typing import MatLike
+import numpy as np
 from getVideo import readVideo
 
 
-def binary(image, method=0, Umin=0, Umax=255) -> MatLike:
+def binary(image, method=0, Umin=0, Umax=255) -> np.ndarray:
     if method == 0:
         ret, imgBinary = cv2.threshold(image, Umin, Umax, cv2.THRESH_BINARY)
     else:
         imgBinary = cv2.inRange(image, Umin, Umax)
-    return imgBinary
+    return imgBinary.astype(np.uint8)
 
 
-def binaryHSV(image, minH=0, maxH=255, minS=0, maxS=255, minV=0, maxV=255) -> MatLike:
+def binaryHSV(
+    image, minH=0, maxH=255, minS=0, maxS=255, minV=0, maxV=255
+) -> np.ndarray:
     imgBinary = cv2.inRange(image, (minH, minS, minV), (maxH, maxS, maxV))
     return imgBinary
 
 
-def frame_contours(frame: MatLike, umin: int, umax: int):
+def frame_contours(frame: np.ndarray, umin: int, umax: int):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     frame_bin = binary(frame_gray, method=0, Umin=umin, Umax=umax)
     # frame_bin_inv = cv2.bitwise_not(frame_bin)
 
@@ -36,10 +39,19 @@ def filter_frame(frame_bin):
 def create_trackbar():
     cv2.namedWindow("Placa")
     cv2.createTrackbar("min", "Placa", 25, 255, lambda x: None)
-    cv2.createTrackbar("max", "Placa", 75, 255, lambda x: None)
+    cv2.createTrackbar("max", "Placa", 80, 255, lambda x: None)
+
+
+def normalize_frame(frame: np.ndarray, desired_light: int = 90) -> np.ndarray:
+    avg = np.average(frame)
+    frame_normalized = frame * (desired_light / avg)
+    return frame_normalized
 
 
 def main() -> None:
+    output_video_path = "binary_video.mp4"
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec para el video de salida
+    out = cv2.VideoWriter(output_video_path, fourcc, 30, (320, 200), isColor=False)
     video_source = "./predicted_video.mp4"
     video = readVideo(src=video_source)
     video.start()
@@ -53,13 +65,17 @@ def main() -> None:
             max = cv2.getTrackbarPos("max", "Placa")
             # contours = frame_contours(frame, 20, 255)
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame_denoised = cv2.fastNlMeansDenoising(frame_gray, None, 10, 10, 21)
-            frame_bin = binary(frame_denoised, method=1, Umin=min, Umax=max)
+            frame_normalized = normalize_frame(frame_gray, 95)
+            frame_bin = binary(frame_normalized, method=1, Umin=min, Umax=max)
             # for cnt in contours: x, y, w, h = cv2.boundingRect(cnt) cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 5)
             cv2.imshow("Placa", cv2.resize(frame_bin, (320, 200)))
+            out.write(cv2.resize(frame_bin, (320, 200)))
         if cv2.waitKey(10) & 0xFF == ord("q"):
             break
+    video.stop()
+    out.release()
     cv2.destroyAllWindows()
+    print(f"Video procesado y guardado en {output_video_path}")
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Quiz 2 Visión artificial
+
+---
+
+## TODO
+
+- [x] Identificar placas
+  - [x] Adaptar el código del profesor para tener imágenes óptimas
+- [/] Segmentar letras
+  - [x] Preprocesamiénto de imágenes
+  - [x] Filtrado de imágenes
+  - [/] Binarización
+  - [ ] Detección de contornos
+  - [ ] Filtrado de contornos
+  - [ ] Segmentación de letras
+- [?] Integrar machine learning
+  - [?] Vectorización de letras
+  - [?] Integración de machine learning
+- [?] Diseñar Interfaz de Usuario
+
+## Arquitectura de software
+
+> [!TODO]
+
+## Segmentación
+
+### Cómo correr
+
+1. Descargar de Teams de la carpeta Quiz_2 bestPlateCar.pt y moverlo a la carpeta del repositorio
+2. Descargar video de los carros y añadir a una nueva carpeta llamada carVideos como 1.mp4 **hasta ahora solo se ha probado el video de Kevin**
+3. Ejecutar detectPlate.py para obtener predicted_video.mp4
+4. Ejecutar binarization.py para obtener binary_video.mp4
+
+En el futuro tanto detectPlate como binarization van a estructurarse como clases que pueden correr en hilos
